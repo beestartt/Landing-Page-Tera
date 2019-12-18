b9a9e3826c27646ea632d941e76e0d5a2854bcb0
Rodrigo Xavier <rodrigoxavier.xavi@gmail.com>
2019-12-17 22:14:13 -0300

    Preparando a landing page

--- assets/js/tera.js
+++ assets/js/tera.js
@@ -1,58 +1,31 @@
 $('#contactform').on('submit', function (e) {
     event.preventDefault();
+
     var nome = $("#name").val();
     var email = $("#email").val();
     var mensagem = $("#message").val();
+
     if (!nome || !email || !mensagem) return;
-    // $.ajax({
-    //     url: 'https://formspree.io/rodrigo@beestart.com.br',
-    //     method: 'POST',
-    //     username: 'api',
-    //     dataType: "json",
-    //     password: 'key-13806c38ee96fe6f7bbcceea30bd3aa4',
-    //     data: {
-    //         name: nome,
-    //         _replyto: email,
-    //         email: email,
-    //         comments: mensagem,
-    //         _subject: '[TERA] Contato através do site!',
-    //     },
-    //     success: function () {
-    //         console.log('success');
-    //         // $('#formBlock').hide();
-    //         // $('#thankyouBlock').show();
-    //     },
-    //     error: function (error) {
-    //         console.log("Error: ", error)
-    //     }
-    // });
 
-    var form = {
-        name: nome,
-        _replyto: email,
-        email: email,
-        comments: mensagem,
-        _subject: '[TERA] Contato através do site!',
-    };
-
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://api.mailgun.net/v3/beestart.com.br/messages",
-        "method": "POST",
-        "headers": {
-            "Authorization": "Basic YXBpOmtleS0xMzgwNmMzOGVlOTZmZTZmN2JiY2NlZWEzMGJkM2FhNA==",
-            "Accept": "*/*",
-            "Cache-Control": "no-cache",
-            "cache-control": "no-cache"
+    $.ajax({
+        url: 'https://us-central1-tera-db-auth-storage.cloudfunctions.net/sendEmailContact',
+        method: 'POST',
+        dataType: "json",
+        data: {
+            from: "noreply@terageosolucoes.com.br",
+            to: email,
+            email: email,
+            name: nome,
+            message: mensagem
         },
-        "processData": false,
-        "contentType": false,
-        "mimeType": "multipart/form-data",
-        "data": form
-    }
-
-    $.ajax(settings).done(function (response) {
-        console.log(response);
+        success: function () {
+            console.log('success');
+            // $('#formBlock').hide();
+            // $('#thankyouBlock').show();
+        },
+        error: function (error) {
+            console.log("Error: ", error)
+        }
     });
+
 });
