b8a568157b9af914fcd1ac858fefbce923f034a4
Rodrigo Xavier <rodrigoxavier.xavi@gmail.com>
2019-12-17 21:04:38 -0300

    Andando com o Mailgun

--- assets/js/tera.js
+++ assets/js/tera.js
@@ -1,30 +1,58 @@
-var nome = $("#name").val();
-var email = $("#email").val();
-var mensagem = $("#message").val();
+$('#contactform').on('submit', function (e) {
+    event.preventDefault();
+    var nome = $("#name").val();
+    var email = $("#email").val();
+    var mensagem = $("#message").val();
+    if (!nome || !email || !mensagem) return;
+    // $.ajax({
+    //     url: 'https://formspree.io/rodrigo@beestart.com.br',
+    //     method: 'POST',
+    //     username: 'api',
+    //     dataType: "json",
+    //     password: 'key-13806c38ee96fe6f7bbcceea30bd3aa4',
+    //     data: {
+    //         name: nome,
+    //         _replyto: email,
+    //         email: email,
+    //         comments: mensagem,
+    //         _subject: '[TERA] Contato através do site!',
+    //     },
+    //     success: function () {
+    //         console.log('success');
+    //         // $('#formBlock').hide();
+    //         // $('#thankyouBlock').show();
+    //     },
+    //     error: function (error) {
+    //         console.log("Error: ", error)
+    //     }
+    // });
 
-$("#sendMessage").on("click", function () {
-    if (!name || !email || !mensagem) return;
-    $.ajax({
-        url: "https://api.mailgun.net/v3/beestart.com.br/messages",
-        type: 'POST',
-        cache: false,
-        headers: {
-            Authorization: "Basic YXBpOmtleS0xMzgwNmMzOGVlOTZmZTZmN2JiY2NlZWEzMGJkM2FhNA==", //! Se a API KEY mudar essa chave deve ser refeita.
-            "content-type":
-                "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+    var form = {
+        name: nome,
+        _replyto: email,
+        email: email,
+        comments: mensagem,
+        _subject: '[TERA] Contato através do site!',
+    };
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.mailgun.net/v3/beestart.com.br/messages",
+        "method": "POST",
+        "headers": {
+            "Authorization": "Basic YXBpOmtleS0xMzgwNmMzOGVlOTZmZTZmN2JiY2NlZWEzMGJkM2FhNA==",
+            "Accept": "*/*",
+            "Cache-Control": "no-cache",
+            "cache-control": "no-cache"
         },
-        username: 'api',
-        password: 'key-13806c38ee96fe6f7bbcceea30bd3aa4',
-        data: { "from": "rodrigo@beestart.com.br", "to": "rodrigo@beestart.com.br", "text": "Contato" },
-        success: function (data) {
-            //somefunctionhere();
-            console.log("Aeeeee")
-        },
-        error: function (data) {
-            console.log('Silent failure.');
-        }
+        "processData": false,
+        "contentType": false,
+        "mimeType": "multipart/form-data",
+        "data": form
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
     });
-    return false;
-
-
 });
